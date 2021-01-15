bcc07a8fac1cb50c4abdc9465987f7a05bd0d577
Russ Magee <rmagee@gmail.com>
2021-01-15 00:31:36 -0800

    Removed static cptest/ dir, now managed by CI tests

---
+++ xc_testfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## setup.sh - create some files for xc copy testing
+dir=cptest
+
+mkdir -p ${dir}/subdir
+dd bs=1024 count=16 if=/dev/urandom of=${dir}/file16KB
+dd bs=1024 count=16 if=/dev/urandom of=${dir}/file1KB
+dd bs=1024 count=16 if=/dev/urandom of=${dir}/file32KB
+dd bs=1024 count=16 if=/dev/urandom of=${dir}/file6B
+dd bs=1048576 count=32 if=/dev/urandom of=${dir}/subdir/file32MB
+dd bs=1048576 count=64 if=/dev/urandom of=${dir}/subdir/file64MB
+
